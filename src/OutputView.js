5146125e2e124df8cdd7554055f2c8cc3228089d
nkEllaaa <dhrtns515@gmail.com>
2023-11-11 23:52:12 +0900

    feat: OutputView 방문 날짜에 따른 이벤트 혜택 안내 문구 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export default OutputView = {
+const OutputView = {
   printWelcom() {
-    Console.print('<안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.>');
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
+
+  printEventBenefitForDate(visitDate) {
+		Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`)
+	},
 
   printMenu() {
     Console.print('<주문 메뉴>');
@@ -33,3 +37,5 @@ export default OutputView = {
     Console.print('<12월 이벤트 배지>');
   },
 };
+
+export default OutputView;
