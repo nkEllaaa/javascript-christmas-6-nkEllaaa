20d89ba0cba68b8a85c0966a4dc14754d954c4e3
nkEllaaa <dhrtns515@gmail.com>
2023-11-13 14:52:52 +0900

    feat: validateVisitDate 방문 날짜 유효성 검사 기능 구현

---
+++ src/validation/validateVisitDate.js
@@ -0,0 +1,19 @@
+import ERROR_DATE from '../constants/error';
+
+const validateVisitDate = (visitDate) => {
+  if (visitDate.length === 0) {
+    throw Error(ERROR_DATE.none);
+  }
+
+  const visitDateToNumber = parseInt(visitDate, 10);
+  if (Number.isNaN(visitDateToNumber)) {
+    throw Error(ERROR_DATE.type);
+  }
+
+  if (visitDateToNumber < 1 || visitDateToNumber > 31) {
+    throw Error(ERROR_DATE.dateRange);
+  }
+  return visitDateToNumber;
+};
+
+export default validateVisitDate;
