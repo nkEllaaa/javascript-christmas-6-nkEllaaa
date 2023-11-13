8245a7071b6be1ef914381fcdcc88a41172fb3df
nkEllaaa <dhrtns515@gmail.com>
2023-11-13 14:50:56 +0900

    feat: menu 주문 메뉴를 담아놓은 객체 기능 구현

---
+++ src/constants/menu.js
@@ -0,0 +1,19 @@
+const MENU = Object.freeze({
+  양송이수프: { type: 'appetizer', price: 6_000 },
+  타파스: { type: 'appetizer', price: 5_500 },
+  시저샐러드: { type: 'appetizer', price: 8_000 },
+
+  티본스테이크: { type: 'main', price: 55_000 },
+  바비큐립: { type: 'main', price: 54_000 },
+  해산물파스타: { type: 'main', price: 35_000 },
+  크리스마스파스타: { type: 'main', price: 25_000 },
+
+  초코케이크: { type: 'dessert', price: 15_000 },
+  아이스크림: { type: 'dessert', price: 5_000 },
+
+  제로콜라: { type: 'beverage', price: 3_000 },
+  레드와인: { type: 'beverage', price: 60_000 },
+  샴페인: { type: 'beverage', price: 25_000 },
+});
+
+export default MENU;
