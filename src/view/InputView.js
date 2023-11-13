d79a68672753cdf2aa9f2d9d7dad2e2be14b26a3
nkEllaaa <dhrtns515@gmail.com>
2023-11-13 14:54:27 +0900

    refactor: 경로 변경으로 인한 파일 재커밋

---
+++ src/view/InputView.js
@@ -0,0 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const InputView = {
+  async getVisitDate() {
+    const visitDate = await Console.readLineAsync(
+      '12월 중 식당 예상 방문 날짜는 언제인가요? (월은 제외하고 날짜만 입력해주세요. e.g. 12)'
+    );
+    const visitDateToNumber = parseInt(visitDate, 10);
+    return visitDateToNumber;
+  },
+
+  async getMenuAndQuantity() {
+    const menuAndQuantity = await Console.readLineAsync(
+      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)'
+    );
+    const menuAndQuantityArray = menuAndQuantity.split(',');
+    return menuAndQuantityArray;
+  },
+};
+
+export default InputView;
---
+++ src/view/OutputView.js
@@ -0,0 +1,47 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMessage(message) {
+    Console.print(message);
+  },
+
+  printWelcom() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+  },
+
+  printEventBenefitForDate(visitDate) {
+    Console.print(
+      `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
+    );
+  },
+
+  printMenu() {
+    Console.print('<주문 메뉴>');
+  },
+
+  printTotalCostBeforeDiscount() {
+    Console.print('<할인 전 총주문 금액>');
+  },
+
+  printBonusMenu() {
+    Console.print('<증정 메뉴>');
+  },
+
+  printBenefitLists() {
+    Console.print('<혜택 내역>');
+  },
+
+  printTotalDiscountCost() {
+    Console.print('<총혜택 금액>');
+  },
+
+  printExpectedCostAfterDiscount() {
+    Console.print('<할인 후 예상 결제 금액>');
+  },
+
+  printEventBadge() {
+    Console.print('<12월 이벤트 배지>');
+  },
+};
+
+export default OutputView;
