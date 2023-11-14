f5b38690e05247bd8ada4e1713b8d0e7b640ffe3
nkEllaaa <dhrtns515@gmail.com>
2023-11-14 18:05:30 +0900

    feat: EventCalculator 주중 이벤트 할인 금액 계산 기능 구현

--- src/controller/EventCalculator.js
+++ src/controller/EventCalculator.js
@@ -37,6 +37,27 @@ class EventCalculator {
     const christMasDiscountAmount = visitDate - 1 * 100 + 1000;
     this.#totalDiscountPrice += christMasDiscountAmount;
   }
+
+  // 주중 이벤트를 위한 디저트 개수
+  getDessertCount() {
+    let dessertCount = 0;
+    this.#orderMenu.forEach((orderItem) => {
+      const SameMenu = MenuList[orderItem.name];
+      if (SameMenu) {
+        dessertCount++;
+      }
+    });
+
+    console.log('토탈', dessertCount);
+    return dessertCount;
+  }
+
+  // 주중 이벤트 계산
+  calculateWeekdayEvent() {
+    const dessertCount = this.getDessertCount();
+    const totalWeekdayDiscountAmount = dessertCount * 2023;
+    return totalWeekdayDiscountAmount;
+  }
 }
 
 export default EventCalculator;
