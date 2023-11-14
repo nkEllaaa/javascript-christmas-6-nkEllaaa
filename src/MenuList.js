d4085d417d77b5e6bc231c07d5e8ac7891a15b6d
nkEllaaa <dhrtns515@gmail.com>
2023-11-14 16:18:03 +0900

    refactor: MenuList 메뉴판으로서 기능할 수 있도록 count 상태 삭제

--- src/MenuList.js
+++ src/MenuList.js
@@ -1,66 +1,55 @@
-class MenuList {
-  static 양송이수프 = {
+const MenuList = {
+  양송이수프: {
     type: 'appetizer',
     price: 6_000,
-    count: 0,
-  };
-  static 타파스 = {
+  },
+  타파스: {
     type: 'appetizer',
     price: 5_500,
-    count: 0,
-  };
-  static 시저샐러드 = {
+  },
+  시저샐러드: {
     type: 'appetizer',
     price: 8_000,
-    count: 0,
-  };
+  },
 
-  static 티본스테이크 = {
+  티본스테이크: {
     type: 'main',
     price: 55_000,
-    count: 0,
-  };
-  static 바비큐립 = {
+  },
+  바비큐립: {
     type: 'main',
     price: 54_000,
-    count: 0,
-  };
-  static 해산물파스타 = {
+  },
+  해산물파스타: {
     type: 'main',
     price: 35_000,
-    count: 0,
-  };
-  static 크리스마스파스타 = {
+  },
+  크리스마스파스타: {
     type: 'main',
-    price: 25_000
-  };
+    price: 25_000,
+  },
 
-  static 초코케이크 = {
+  초코케이크: {
     type: 'dessert',
     price: 15_000,
-    count: 0,
-  };
-  static 아이스크림 = {
+  },
+  아이스크림: {
     type: 'dessert',
     price: 5_000,
-    count: 0,
-  };
+  },
 
-  static 제로콜라 = {
+  제로콜라: {
     type: 'beverage',
     price: 3_000,
-    count: 0,
-  };
-  static 레드와인 = {
+  },
+  레드와인: {
     type: 'beverage',
     price: 60_000,
-    count: 0,
-  };
-  static 샴페인 = {
+  },
+  샴페인: {
     type: 'beverage',
     price: 25_000,
-    count: 0,
-  };
+  },
 }
 
 export default MenuList;
