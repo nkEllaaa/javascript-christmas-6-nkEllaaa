3f8929f89666eaaa07b72b6f7e68a0ed7f4daee9
nkEllaaa <dhrtns515@gmail.com>
2023-11-14 17:38:43 +0900

    feat: EventManager 유효한 이벤트를 판단하고 반환하는 기능 구현

--- src/controller/EventManager.js
+++ src/controller/EventManager.js
@@ -16,6 +16,20 @@ class EventManager {
     };
   }
 
+  getEventList() {
+    return this.#eventList;
+  }
+
+  formatDateAndjudgeValidEvents(totalPrice) {
+    this.formatDateTwoDigits();
+    this.formatDateToDay();
+    
+    this.checkChristmasEvent();
+    this.checkweekdayOrEndEvent();
+    this.checkSpecialEvent();
+    this.checkChampagne(totalPrice);
+  }
+
   //앱으로부터 방문 날짜를 받음
   getVisitDateNumber(visitDateNumber) {
     this.#visitDateNumber = visitDateNumber;
@@ -33,5 +47,36 @@ class EventManager {
     console.log('포메티드대이트', formattedDate);
     this.#visitDayNumber = visitDayNumber;
   }
+
+  //크리스마스 이벤트 판단
+  checkChristmasEvent() {
+    if (this.#visitDateNumber >= 1 && this.#visitDateNumber <= 25) this.#eventList.christmas = true;
+  }
+
+  //주말,주중 이벤트 판단
+  checkweekdayOrEndEvent() {
+    if (this.#visitDayNumber === 5 || this.#visitDayNumber === 6) {
+      this.#eventList.weekend = true;
+      this.#eventList.weekday = false;
+    } else {
+      this.#eventList.weekday = true;
+      this.#eventList.weekend = false;
+    }
+  }
+
+  //스페셜 이벤트 판단
+  checkSpecialEvent() {
+    if (this.#visitDayNumber === 0 || this.#visitDateNumber === 25) this.#eventList.special = true;
+  }
+
+  //샴페인 증정 이벤트 판단
+  checkChampagne(totalPrice) {
+    if (totalPrice >= 120000) this.#eventList.champagne = true;
+  }
+
+  //배지 이벤트 판단
+  // checkBadgeEvent() {
+  // 계산기에서 총 할인 금액 가져와서 금액에 따라 배지 초기화
+  // }
 }
 export default EventManager;
