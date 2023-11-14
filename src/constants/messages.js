abf91742891d30c7a98d800426976c2e809de7af
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 02:36:57 +0900

    refactof: messages 타이틀의 <> 삭제

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,13 +1,13 @@
 const MESSAGES = {
   nothing: '없음',
   welcome: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
-  menu: '<주문 메뉴>',
-  totalPriceBeforeDiscount: '<할인 전 총주문 금액>',
-  bonusMenu: '<증정 메뉴>',
-  benefitLists: '<혜택 내역>',
+  menu: '주문 메뉴',
+  totalPriceBeforeDiscount: '할인 전 총주문 금액',
+  bonusMenu: '증정 메뉴',
+  benefitLists: '혜택 내역',
   totalDiscountCost: '총혜택 금액',
-  expectedCostAfterDiscount: '<할인 후 예상 결제 금액>',
-  badge: '<12월 이벤트 배지>',
+  expectedCostAfterDiscount: '할인 후 예상 결제 금액',
+  badge: '12월 이벤트 배지',
 
   eventForDate(visitDate) {
     `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`;
