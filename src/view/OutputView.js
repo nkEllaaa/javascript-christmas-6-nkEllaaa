ab4a5a656089ee938700c4cb2b194ce5495a69d5
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 02:40:40 +0900

    refactor: OutputView 중복되는 형식을 함수로 작성

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,42 +5,35 @@ const OutputView = {
     Console.print(message);
   },
 
+  printTitle(title) {
+    Console.print(`<${title}>\n`);
+  },
+
+  printMenuAndCount(menuName, menuCount) {
+    Console.print(`${menuName} ${menuCount}개\n`);
+  },
+
+  printTotalDiscountList(eventName, discount) {
+    const formatDiscount = price.toLocaleString('ko-KR');
+    Console.print(`${eventName}: -${formatDiscount}원\n`);
+  },
+
+  printPrice(price) {
+    const formatPrice = price.toLocaleString('ko-KR');
+    Console.print(`${formatPrice}원\n`);
+  },
+
+  printTotalDiscountAmount(discount) {
+    const formatDiscount = discount.toLocaleString('ko-KR');
+    Console.print(`-${formatDiscount}원\n`);
+  },
+
   printWelcom() {
-    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n');
   },
 
-  printEventBenefitForDate(visitDate) {
-    Console.print(
-      `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
-    );
-  },
-
-  printMenu() {
-    Console.print('<주문 메뉴>');
-  },
-
-  printTotalCostBeforeDiscount() {
-    Console.print('<할인 전 총주문 금액>');
-  },
-
-  printBonusMenu() {
-    Console.print('<증정 메뉴>');
-  },
-
-  printBenefitLists() {
-    Console.print('<혜택 내역>');
-  },
-
-  printTotalDiscountCost() {
-    Console.print('<총혜택 금액>');
-  },
-
-  printExpectedCostAfterDiscount() {
-    Console.print('<할인 후 예상 결제 금액>');
-  },
-
-  printEventBadge() {
-    Console.print('<12월 이벤트 배지>');
+  printDiscountForDate(visitDate) {
+    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
   },
 };
 
