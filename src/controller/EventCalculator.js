bbe2df7b0e2afdc5986ad058b6b0801a72eea396
nkEllaaa <dhrtns515@gmail.com>
2023-11-14 20:04:06 +0900

    feat: EventCalculator 스페셜,샴페인,배지이벤트 할인 금액 계산 기능 구현

--- src/controller/EventCalculator.js
+++ src/controller/EventCalculator.js
@@ -37,8 +37,8 @@ class EventCalculator {
   }
 
   // 크리스마스 이벤트 계산
-  calculateChristmasEvent(visitDate) {
-    const christMasDiscountAmount = visitDate - 1 * 100 + 1000;
+  calculateChristmasEvent() {
+    const christMasDiscountAmount = this.#visitDate - 1 * 100 + 1000;
     this.#totalDiscountPrice += christMasDiscountAmount;
   }
 
@@ -62,13 +62,36 @@ class EventCalculator {
   // 주중 이벤트 계산
   calculateWeekdayEvent() {
     const weekdayDiscountAmount = this.#dessertCount * 2023;
-    return weekdayDiscountAmount;
+    this.#totalDiscountPrice += weekdayDiscountAmount;
   }
 
   // 주말 이벤트 계산
   calculateWeekendEvent() {
     const weekendDiscountAmount = this.#mainCount * 2023;
-    return weekendDiscountAmount;
+    this.#totalDiscountPrice += weekendDiscountAmount;
+  }
+
+  // 스페셜 이벤트 계산
+  calculateSpecialEvent() {
+    if (this.#eventList.special === true) {
+      const specialDiscountAmount = 1000;
+      this.#totalDiscountPrice += specialDiscountAmount;
+    }
+  }
+
+  // 샴페인 이벤트 계산
+  calculateChampagneEvent() {
+    if (this.#totalPrice >= 120000) {
+      const champagneDiscountAmount = 25000;
+      this.#totalDiscountPrice += champagneDiscountAmount;
+    }
+  }
+
+  // 배지 이벤트 계산
+  calculateBadgeEvent() {
+    if (this.#totalDiscountPrice >= 20000) this.#eventList.badge = '산타';
+    else if (this.#totalDiscountPrice >= 10000) this.#eventList.badge = '트리';
+    else if (this.#totalDiscountPrice >= 5000) this.#eventList.badge = '별';
   }
 }
 
