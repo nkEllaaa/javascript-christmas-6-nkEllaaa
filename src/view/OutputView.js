1f5ef1f90d965671a4e1954f641f8791244afead
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 20:22:40 +0900

    fix: OuputView 개행문자로 인해 테스트 통과하지 못하는 오류

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,20 +2,20 @@ import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
   printMessage(message) {
-    Console.print(message, '\n');
+    Console.print(message);
   },
 
   printTitle(title) {
-    Console.print(`<${title}>\n`);
+    Console.print(`<${title}>`);
   },
 
   printMenuAndCount(menuName, menuCount) {
-    Console.print(`${menuName} ${menuCount}개\n`);
+    Console.print(`${menuName} ${menuCount}개`);
   },
 
   printTotalDiscountList(eventName, discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`${eventName}: -${formatDiscount}원\n`);
+    Console.print(`${eventName}: -${formatDiscount}원`);
   },
 
   printPrice(price) {
@@ -25,11 +25,11 @@ const OutputView = {
 
   printTotalDiscountAmount(discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`-${formatDiscount}원\n`);
+    Console.print(`-${formatDiscount}원`);
   },
 
   printDiscountForDate(visitDate) {
-    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
+    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
   },
 };
 
