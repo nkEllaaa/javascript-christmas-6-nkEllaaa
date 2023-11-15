cb50cf79fc0d292cd46318f53a7a98b1d230d30c
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 23:19:55 +0900

    fix: OutputView  개행 삭제

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,30 +6,30 @@ const OutputView = {
   },
 
   printTitle(title) {
-    Console.print(`<${title}>\n`);
+    Console.print(`<${title}>`);
   },
 
   printMenuAndCount(menuName, menuCount) {
-    Console.print(`${menuName} ${menuCount}개\n`);
+    Console.print(`${menuName} ${menuCount}개`);
   },
 
   printTotalDiscountList(eventName, discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`${eventName}: -${formatDiscount}원\n`);
+    Console.print(`${eventName}: -${formatDiscount}원`);
   },
 
   printPrice(price) {
     const formatPrice = price.toLocaleString('ko-KR');
-    Console.print(`${formatPrice}원\n`);
+    Console.print(`${formatPrice}원`);
   },
 
   printTotalDiscountAmount(discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`-${formatDiscount}원\n`);
+    Console.print(`-${formatDiscount}원`);
   },
 
   printDiscountForDate(visitDate) {
-    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
+    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
   },
 };
 
