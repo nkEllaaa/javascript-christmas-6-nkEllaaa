0c0af4cc318f7e6d78aa37c712c31bba43df5169
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 19:14:33 +0900

    refactor: console.log 삭제 누락 부분 추가 삭제

--- src/controller/EventPlanner.js
+++ src/controller/EventPlanner.js
@@ -9,7 +9,6 @@ class EventPlanner {
   async getVisitDate() {
     const visitDateNumber = await InputView.getVisitDate();
     try {
-      console.log('방문 날짜', visitDateNumber);
       validateVisitDate(visitDateNumber);
       return visitDateNumber;
     } catch (error) {
@@ -21,7 +20,6 @@ class EventPlanner {
   async getOrderMenu() {
     const orderMenuArray = await InputView.getOrderMenu();
     try {
-      console.log('주문 메뉴와 수량', orderMenuArray);
       validateMenu(orderMenuArray);
       return orderMenuArray;
     } catch (error) {
