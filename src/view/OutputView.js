2e6116ecfd57df8f81574749198e1505228b145b
nkEllaaa <dhrtns515@gmail.com>
2023-11-15 23:14:27 +0900

    fix: OutputView 개행문자 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,16 +6,16 @@ const OutputView = {
   },
 
   printTitle(title) {
-    Console.print(`<${title}>`);
+    Console.print(`<${title}>\n`);
   },
 
   printMenuAndCount(menuName, menuCount) {
-    Console.print(`${menuName} ${menuCount}개`);
+    Console.print(`${menuName} ${menuCount}개\n`);
   },
 
   printTotalDiscountList(eventName, discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`${eventName}: -${formatDiscount}원`);
+    Console.print(`${eventName}: -${formatDiscount}원\n`);
   },
 
   printPrice(price) {
@@ -25,11 +25,11 @@ const OutputView = {
 
   printTotalDiscountAmount(discount) {
     const formatDiscount = discount.toLocaleString('ko-KR');
-    Console.print(`-${formatDiscount}원`);
+    Console.print(`-${formatDiscount}원\n`);
   },
 
   printDiscountForDate(visitDate) {
-    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+    Console.print(`12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
   },
 };
 
